18ed2c0323aa4a4ee7b2aa599305ccc0329c8e65
Thundercat <michael.c.haines@gmail.com>
2014-11-09 12:38:43 -0700

    keystone items, save button, and loading spinners.

--- app.js
+++ app.js
@@ -4,31 +4,90 @@ var angularApp = angular.module('xsellMockApp', []);
 
 angularApp.controller('xsellMockController', function($scope, $http) {
 
-	$scope.slot1 = []
-	$scope.slot2 = []
-	$scope.slot3 = []
+	bcsUrl = 'http://www.backcountry.com';
+
+	$scope.resetKeystone = function() {
+		$scope.keystone = {};
+		$scope.keystoneInput = '';
+		$scope.keystone.imageUrl = 'alta.jpg';
+	};
 	
+	$scope.resetSlots = function() {
+		$scope.slot1 = [{}, {}, {}];
+		$scope.slot2 = [{}, {}, {}];
+		$scope.slot3 = [{}, {}, {}];
+	};
+	$scope.resetSlots();
+	$scope.resetKeystone();
 
 
+	$scope.keystoneInputToUpper = function(){
+		$scope.keystoneInput = $scope.keystoneInput.toUpperCase();
+	};
+	
+	$scope.getKeystone = function(style){
+		style = style.toUpperCase();
+		if (style != $scope.keystone.style) {
+
+			$scope.resetSlots();
+			$scope.keystone = {};
+			$scope.keystone.imageUrl = 'spinner.gif';
+			$scope.keystone.style = style;
+
+			$http.get('http://localhost:5000/getStyle?style=' + style).success(
+				function(data) {
+					console.log('found some data for style' + style);
+					console.log(data);
+
+					product = data.products[0];
+					sku = product.skus[0];
+
+					$scope.keystone.title = product.title;
+					$scope.keystone.url = bcsUrl + sku.url;
+					$scope.keystone.imageUrl = bcsUrl + sku.image.url;
+					$scope.keystone.brand = product.brand.name;
+
+					}).error(function(data, status, headers, config){
+						$scope.keystone.imageUrl = 'asdf';
+						console.log($scope.keystone);
+						$scope.keystone.title = "Error finding sku. Try again.";
+					});
+	
+		};
+	};
+
+	
 	$scope.getRecommendation = function(slot, queryString){
-		recommendationData = $http.get('http://localhost:5000/getRecommendations?query=' + queryString).success(
-			function(data) {
-				l = slot.length;
-				console.log('slot beginning at:');
-				console.log(l)
-				for (var i = 0; i < l; i++) {
-					slot.pop();
-					console.log(slot)
-				};
-				
-				data.products.forEach(function(product) {
-					console.log(product);
-					slot.push({'title': product.title, 'brand': product.brand.name, 'url': product.skus[0].url, 'imageUrl': product.skus[0].image.url});
-					console.log(slot);
-				});
-
-
+		if (queryString != slot.lastQuery) {
+			slot.lastQuery = queryString;
+			console.log('looking for recommendation: ' + queryString);
+	
+			slot.forEach(function(product) {
+				product.imageUrl = 'spinner.gif';
+				console.log(product);
 			});
+	
+			recommendationData = $http.get('http://localhost:5000/getRecommendations?query=' + queryString).success(
+				function(data) {
+					console.log('found some data after searching for ' + queryString);
+					console.log(data);
+					l = slot.length;
+					console.log('slot beginning at:');
+					console.log(l)
+						for (var i = 0; i < l; i++) {
+							slot.pop();
+						};
+	
+					data.products.forEach(function(product) {
+						console.log(product);
+	
+						slot.push({'title': product.title, 'brand': product.brand.name, 'url': bcsUrl + product.skus[0].url, 'imageUrl': bcsUrl + product.skus[0].image.url});
+						console.log(slot);
+					});
+	
+	
+				});
+		};
 	};
 
 });
--- server_flask.py
+++ server_flask.py
@@ -11,25 +11,59 @@ def index():
 
 @app.route('/getRecommendations', methods = ['GET'])
 def getRecommendations():
-    query = request.args.get('query')
-
-    if not query:
-        return ('Please specify a query parameter, like: <br> ../getRecommendations?query="Jackets"', 400)
-
-    conn = httplib.HTTPConnection('www.backcountry.com')
-    encoded_query = urllib.quote(query)
-    conn.request('GET', '/v1/products?site=bcs&limit=3&metadata=false&q=' + encoded_query)
-    res = conn.getresponse()
-    data = res.read()
-
     responseHeaders = {'Access-Control-Allow-Origin': '*'}
+    try:
+        query = request.args.get('query')
+        if not query:
+            return ('Request must provvide a search term', 400, responseHeaders)
 
-    if res.status == 200:
-        return (data, 200, responseHeaders)
+        if query == 'undefined':
+            return ('Do not make your request with "undefined" as the query', 400, responseHeaders)
 
-    else:
-        return ('Encountered error with product api.' + res.status + res.reason, 400)
+        conn = httplib.HTTPConnection('www.backcountry.com')
+        encoded_query = urllib.quote(query)
+        conn.request('GET', '/v1/products?site=bcs&limit=3&metadata=false&q=' + encoded_query)
+        res = conn.getresponse()
+        data = res.read()
 
+
+        if res.status == 200:
+            return (data, 200, responseHeaders)
+
+        else:
+            return ('Product not found.' + str(res.status) + str(res.reason), 400, responseHeaders)
+    except:
+        return ('Error', 500, responseHeaders)
+
+
+@app.route('/getStyle', methods=['GET'])
+def getStyle():
+    responseHeaders = {'Access-Control-Allow-Origin': '*'}
+    try:
+        style = request.args.get('style')
+
+        if not style:
+            return ('Request must provide a style argument', 400, responseHeaders)
+
+        if style == 'undefined':
+            return ('Do not make your request with "undefined" as the style', 400, responseHeaders)
+
+        conn = httplib.HTTPConnection('www.backcountry.com')
+        encoded_style = urllib.quote(style)
+
+        conn.request('GET', '/v1/products/' + encoded_style + '?fields=id,skus.image.url,title,seoUrlToken,skus.url,brand')
+        res = conn.getresponse()
+        data = res.read()
+
+
+        if res.status == 200:
+            return (data, 200, responseHeaders)
+
+        else:
+            return ('Encountered error with product api.' + str(res.status) + str(res.reason), 400, responseHeaders)
+
+    except:
+        return ('Error', 500, responseHeaders)
 
 if __name__ == '__main__':
     app.run(debug=True)
